fec9a765d2b62760388e635d9a94fa74d4aaaeee
UMERSIDDIQUI-GUTech <umer13sep@gmail.com>
2024-12-23 13:59:07 +0500

    lab-10 2nd commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div><h1>Contact me</h1></div>
+  )
+}
+
+export default contact;
---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div><h1>hello</h1></div>
+  )
+}
+
+export default home;
